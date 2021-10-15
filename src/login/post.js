5e0480915738f1b58648494118d2b8380062e5b6
Eduardo Pérez <eduardojoseperezf@gmail.com>
2021-10-15 18:47:14 -0300

    Refresh token route

--- src/login/post.js
+++ src/login/post.js
@@ -39,7 +39,6 @@ module.exports.loginUser = async (event) => {
   var loginRes;
   try {
     loginRes = await asyncAuthenticateUser(cognitoUser, authenticationDetails);
-    console.log(loginRes);
     if (!loginRes.idToken) {
       error = true;
       message = "PasswordChangeRequired";
@@ -80,6 +79,80 @@ module.exports.loginUser = async (event) => {
     ),
   };
 };
+
+module.exports.refreshToken = async (event) => {
+  // Parse and configure claims and data
+  var status = 200;
+  var message = "ok";
+  var result = {};
+  var error = false;
+  const data = JSON.parse(event.body);
+
+  // Configure Cognito
+  var poolData = {
+    UserPoolId: process.env.AWS_COGNITO_USERPOOLID,
+    ClientId: process.env.AWS_COGNITO_CLIENTID
+  };
+  var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+
+  // Initialize data
+  var userData = {
+  	Username: data.email,
+  	Pool: userPool,
+  };
+  var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+  const cognitoRefreshToken = new AmazonCognitoIdentity.CognitoRefreshToken({RefreshToken: data.token});
+
+  var idToken = "";
+  var refreshToken = "";
+  var accessToken = "";
+  var tokens = {};
+  var refreshRes;
+  try {
+    refreshRes = await asyncRefreshSession(cognitoUser, cognitoRefreshToken);
+    idToken = refreshRes.idToken.jwtToken;
+    refreshToken = refreshRes.refreshToken.token;
+    accessToken = refreshRes.accessToken.jwtToken;
+    result = {
+      "idToken": idToken,
+      "refreshToken": refreshToken,
+      "accessToken": accessToken
+    };
+  } catch (e) {
+    console.log("ERROR");
+    console.log(e);
+    message = e.name;
+    error = true;
+    status = 200;
+  }
+  return {
+    statusCode: status,
+    headers: {
+      "Access-Control-Allow-Origin": "*"
+    },
+    body: JSON.stringify(
+      {
+        result: result,
+        error: error,
+        message: message
+      },
+      null,
+      2
+    ),
+  };
+};
+
+function asyncRefreshSession(cognitoUser, cognitoRefreshToken) {
+  return new Promise(function(resolve, reject) {
+    cognitoUser.refreshSession(cognitoRefreshToken, (err, session)=>{
+      if (err) {
+        reject(err);
+      } else {
+        resolve(session);
+      }
+    });
+  });
+}
 
 function asyncAuthenticateUser(cognitoUser, cognitoAuthenticationDetails) {
   return new Promise(function(resolve, reject) {
