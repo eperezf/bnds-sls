cd3a0e23ad38d7d531e61f718379d671d3be9d40
Eduardo Pérez <eduardojoseperezf@gmail.com>
2021-10-16 15:50:51 -0300

    Upload image on add

--- src/operator/post.js
+++ src/operator/post.js
@@ -17,7 +17,10 @@ module.exports.createOperator = async (event) => {
   // Configure DynamoDB
   const tableName = "operators-"+process.env.NODE_ENV;
   const client = new DynamoDBClient({region: "us-east-1", endpoint: process.env.DYNAMODB_ENDPOINT});
-  const docClient = DynamoDBDocument.from(client);
+  const docClient = DynamoDBDocument.from(client, {marshallOptions:{removeUndefinedValues:true}});
+
+  // Configure S3
+  const S3client = new S3Client();
 
   // Validate that all the data is correct
   if (!data.name || data.name == "") {
@@ -33,6 +36,7 @@ module.exports.createOperator = async (event) => {
   }
 
   try {
+    // Try saving the operator
     let id = nanoid(6);
     let params = {
       TableName: tableName,
@@ -46,52 +50,25 @@ module.exports.createOperator = async (event) => {
         frequencies: data.frequencies
       }
     };
+    // Save the operator
     let operator = await docClient.put(params);
     result = operator;
+    // Create the S3 command
+    const command = new PutObjectCommand({
+      Bucket:process.env.AWS_S3_BUCKET_NAME,
+      Key:"operators/"+id+".png",
+      ACL: 'public-read'
+    });
+    // Get the signed URL
+    const url = await getSignedUrl(S3client, command, {expiresIn: 600});
+    result.uploadUrl=url;
+
   } catch (e) {
     console.log(e);
     message = "Error agregando Operadora";
     error = e;
     status = 500;
   }
-
-  // Return the data
-  return {
-    statusCode: status,
-    headers: {
-      "Access-Control-Allow-Origin": "*"
-    },
-    body: JSON.stringify(
-      {
-        result: result,
-        error: error,
-        message: message
-      },
-      null,
-      2
-    ),
-  };
-};
-
-module.exports.uploadOperatorImage = async (event) => {
-  // Parse and configure claims and data
-  var status = 200;
-  var message = "ok";
-  var result = {};
-  var error = false;
-  const data = JSON.parse(event.body);
-  console.log("Configuring S3");
-  const client = new S3Client();
-  console.log("Client set");
-  const command = new PutObjectCommand({
-    Bucket:"static-dev.bnds.cl",
-    Key:"operators/id",
-    ACL: 'public-read'
-  });
-  console.log("Command set");
-  console.log("Getting signed URL");
-  const url = await getSignedUrl(client, command, {expiresIn: 600});
-  console.log(url);
 
   // Return the data
   return {
