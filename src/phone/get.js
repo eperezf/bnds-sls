db8308566cdb2e39014c561beec897c44ecf6adb
Eduardo Pérez <eduardojoseperezf@gmail.com>
2021-10-17 22:05:49 -0300

    Phone basic add and edit

--- src/phone/get.js
+++ src/phone/get.js
@@ -3,6 +3,78 @@ const { DynamoDBDocument, GetCommand } = require("@aws-sdk/lib-dynamodb");
 const { DynamoDBClient} = require("@aws-sdk/client-dynamodb");
 const { Client } = require('@elastic/elasticsearch');
 
+module.exports.getPhone = async (event) => {
+  // Parse and configure claims and data
+  var status = 200;
+  var message = "ok";
+  const data = event.pathParameters;
+  console.log(data);
+  var phoneData = {};
+  phoneData.variants = [];
+
+  // Configure DynamoDB
+  const tableName = "phones-"+process.env.NODE_ENV;
+  const dynamoClient = new DynamoDBClient({region: "us-east-1", endpoint: process.env.DYNAMODB_ENDPOINT});
+  const docClient = DynamoDBDocument.from(dynamoClient);
+
+  // Get phone data
+  var params = {
+    TableName: tableName,
+    KeyConditionExpression: "#pk = :pk",
+    ExpressionAttributeValues: {
+      ":pk":"PHONE#"+event.pathParameters.id
+    },
+    ExpressionAttributeNames: {
+      "#pk":"PK"
+    }
+  };
+  try {
+    var result = await docClient.query(params);
+    if (result.Items[0]) {
+      for (var phone of result.Items) {
+        console.log(phone);
+        if (phone.SK == "DATA") {
+          phoneData.id = phone.PK.replace("PHONE#","");
+          phoneData.brand = phone.brand;
+          phoneData.model = phone.model;
+          phoneData.enabled = phone.enabled;
+        }
+        else if (phone.SK.startsWith("VARIANT")) {
+          phone.id = phone.SK.replace("VARIANT#","");
+          delete phone.SK;
+          delete phone.PK;
+          phoneData.variants.push(phone);
+        }
+      }
+    } else {
+      status = 404;
+      message = "Phone not found";
+    }
+  } catch (e) {
+    console.log(e);
+    status = 500;
+    message = e;
+  }
+  // Return the data
+  return {
+    statusCode: status,
+    headers: {
+      "Access-Control-Allow-Origin": "*"
+    },
+    body: JSON.stringify(
+      {
+        phone: phoneData,
+        message: message
+      },
+      null,
+      2
+    ),
+  };
+
+}
+
+
+
 module.exports.getPhoneData = async (event) => {
 
   // Parse and configure claims and data
--- src/phone/post.js
+++ src/phone/post.js
@@ -2,7 +2,9 @@
 
 const { DynamoDBDocument, GetCommand } = require("@aws-sdk/lib-dynamodb");
 const { DynamoDBClient} = require("@aws-sdk/client-dynamodb");
-const { Client } = require('@elastic/elasticsearch');
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+const nanoid = require ('nanoid');
 
 module.exports.createPhone = async (event) => {
   // Parse and configure claims and data
@@ -15,47 +17,56 @@ module.exports.createPhone = async (event) => {
   // Configure DynamoDB
   const tableName = "phones-"+process.env.NODE_ENV;
   const client = new DynamoDBClient({region: "us-east-1", endpoint: process.env.DYNAMODB_ENDPOINT});
-  const docClient = DynamoDBDocument.from(client);
+  const docClient = DynamoDBDocument.from(client, {marshallOptions:{removeUndefinedValues:true}});
 
-  //Configure Elasticsearch
-  const esclient = new Client({
-  node: process.env.ELASTIC_ENDPOINT,
-  auth: {
-    username: process.env.ELASTIC_USERNAME,
-    password: process.env.ELASTIC_PASSWORD
+  // Configure S3
+  const S3client = new S3Client();
+
+  // Validate that all the data is correct
+  if (!data.brand || data.brand == "") {
+    error = true;
+    message = "Missing parameters";
+    status = 500;
   }
-  });
 
-  let params = {
-    TableName: tableName,
-    Item: {
-      PK: data.brand,
-      SK: data.model,
-      imgUrl: data.imgUrl,
-      reviewUrl: data.reviewUrl,
-      variants: data.variants
-    }
-  };
+  if (!data.model || data.model == "") {
+    error = true;
+    message = "Missing parameters";
+    status = 500;
+  }
+
   try {
-    var phoneData = await docClient.put(params);
+    // Try saving the operator
+    let id = nanoid(6);
+    let params = {
+      TableName: tableName,
+      Item: {
+        PK: "PHONE#"+id,
+        SK: "DATA",
+        brand: data.brand,
+        model: data.model,
+        enabled: data.enabled,
+      }
+    };
+    // Save the phone
+    let phone = await docClient.put(params);
+    result = phone;
+    // Create the S3 command
+    const command = new PutObjectCommand({
+      Bucket:process.env.AWS_S3_BUCKET_NAME,
+      Key:"phones/"+id+".png",
+      ACL: 'public-read'
+    });
+    // Get the signed URL
+    const url = await getSignedUrl(S3client, command, {expiresIn: 600});
+    result.uploadUrl=url;
+
   } catch (e) {
     console.log(e);
-    error = true;
-    message = e;
+    message = "Error agregando Teléfono";
+    error = e;
+    status = 500;
   }
-
-  data.variants.forEach((variant, i) => {
-    esclient.index({
-      index: 'phones',
-      body: {
-        fullName: data.brand + " " + data.model + " " + variant.variant,
-        PK: data.brand,
-        SK: data.model,
-        variant: variant.variant,
-        enabled: variant.enabled
-      }
-    });
-  });
 
   // Return the data
   return {
